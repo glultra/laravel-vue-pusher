28701e717596f5b2977686ec9e18bfaa0f512307
glultra <7amakurd2002@gmail.com>
2023-10-27 14:11:48 +0300

    Setup vue.

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    Slaw mamosta
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "App"
+})
+</script>
+
+<style scoped>
+
+</style>
